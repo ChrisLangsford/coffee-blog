2d676c3b4096a5fea5983a5d7617091b8c8ff4be
Christopher Langsford <langz020@gmail.com>
2016-04-27 09:27:16 +0200

    clarified path to home and index

--- routes/index.js
+++ routes/index.js
@@ -13,10 +13,9 @@ var isAuthenticated = function (req, res, next) {
 
 module.exports = function (passport) {
 
-  /* GET login page. */
+
   router.get('/', function (req, res) {
-    // Display the Login page with any flash message, if any
-    res.render('index', { message: req.flash('message') });
+    res.render('index');
   });
 
   /* Handle Login POST */
